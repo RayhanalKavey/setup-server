0b7134fe27552281ffe777c4b76578d1aa996d7c
RayhanalKavey <rayhanalkavey@gmail.com>
2023-02-13 08:32:26 +0600

    Initial server setup with database connection. All we need to provide the useী name,port, and password to the dotenile for mongodb connection.

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+require("dotenv").config();
+require("colors");
+
+const app = express();
+const port = process.env.PORT || 5005;
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+// Db connections
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hufticd.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function dbConnect() {
+  try {
+    await client.connect();
+    console.log("Database connected!".cyan.bgWhite);
+  } catch (error) {
+    console.log(error.name.bgWhite.red, error.message.red);
+  }
+}
+dbConnect();
+
+async function run() {
+  try {
+    // const blogCollection = client.db("contentManager").collection("blogs");
+    // app.post("/blog-add", async (req, res) => {
+    //   const blog = req.body;
+    //   const result = await blogCollection.insertOne(blog);
+    //   res.send(result);
+    // });
+    // app.get(`/blogs`, async (req, res) => {
+    //   const query = {};
+    //   const blogs = await blogCollection.find(query).toArray();
+    //   res.send(blogs);
+    // });
+    // app.delete("/blog-delete/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const result = await blogCollection.deleteOne(query);
+    //   res.send(result);
+    // });
+    // app.put("/blog-edit/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const blog = req.body;
+    //   const filter = { _id: new ObjectId(id) };
+    //   const options = { upsert: true };
+    //   const updatedDoc = {
+    //     $set: blog,
+    //   };
+    //   const result = await blogCollection.updateOne(
+    //     filter,
+    //     updatedDoc,
+    //     options
+    //   );
+    //   res.send(result);
+    // });
+  } finally {
+  }
+}
+run();
+// const blog = require("./blog-post");
+// app.get("/blog-post", (req, res) => {
+//   res.send(blog);
+// });
+app.get("/", (req, res) => {
+  res.send("Welcome to the  server.");
+});
+
+app.listen(port, () => {
+  console.log(`Server in running on port: ${port}`.rainbow.bgWhite);
+});
